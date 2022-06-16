65777ce7b6fe217008ca732047ae0d2e9c65ac4c
priyanshiisingh <priyanshianamika1815@gmail.com>
2022-06-16 13:42:25 +0530

    frontend added v1

--- index.js
+++ index.js
@@ -1,42 +1,99 @@
 import nodemailer from "nodemailer";
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import ejs from "ejs";
+import bodyParser from "body-parser";
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.set("public", path.join(__dirname, "public"));
+// app.set("view engine", "ejs");
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.render(__dirname + "/views/index.ejs");
+});
 
 //Using SMTP for Nodemailer Transport
 // let transport = nodemailer.createTransport(options[, defaults])
 
-let testAccount = await nodemailer.createTestAccount();
+app.post("/", (req, res) => {
+  console.log(req.body);
+  const { content, name, email, subject } = req.body;
+  const emailContent = `
+    <h3>Contact Info</h3>
+    <ul>
+        <li>Name: ${name}</li>
+        <li>Email: ${email}</li>
+        <li>Message: ${content} </li>
+    </ul>
+    <h3>Message finished</h3>
+  `;
 
-//Testing our Code with Ethereal
-const transporter = nodemailer.createTransport({
-  host: "smtp.ethereal.email",
-  port: 587,
-  auth: {
-    user: "kiel.lind27@ethereal.email",
-    pass: "25YnN3UFqVbydzVeRs",
-  },
-  //to remove authorization error
-  tls: {
-    rejectUnauthorized: false,
-  },
-});
+  async function main() {
+    nodemailer.createTestAccount((err, account) => {
+      if (err) {
+        console.error("Failed to create a testing account. " + err.message);
+        return process.exit(1);
+      }
 
-const message = {
-  from: "priyanshi.sachan@iwebcode.net", // Sender address
-  to: "priyanshianamika1815@gmail.com", // List of recipients
-  subject: "Design Your Model S | Tesla", // Subject line
-  html: "<h1>Have the most fun you can in a car!</h1><p>Get your <b>Tesla</b> today!</p>", // Plain text body or html script
-  attachments: [
-    {
-      // Use a URL as an attachment
-      filename: "your-testla.png",
-      path: "https://media.gettyimages.com/photos/view-of-tesla-model-s-in-barcelona-spain-on-september-10-2018-picture-id1032050330?s=2048x2048",
-    },
-  ],
-};
-transporter.sendMail(message, function (err, info) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(info);
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+      console.log("Credentials obtained, sending message...");
+
+      //Testing our Code with Ethereal
+      const transporter = nodemailer.createTransport({
+        host: "smtp.ethereal.email",
+        port: 587,
+        auth: {
+          user: account.user,
+          pass: account.pass,
+        },
+        //to remove authorization error
+        tls: {
+          rejectUnauthorized: false,
+        },
+      });
+
+      const message = {
+        from: "priyanshi.sachan@iwebcode.net", // Sender address
+        to: "priyanshianamika@gmail.com", // List of recipients
+        subject: `${subject}`, // Subject line
+        html: emailContent, // Plain text body or html script
+        attachments: [
+          {
+            // Use a URL as an attachment
+            filename: "your-testla.png",
+            path: "https://media.gettyimages.com/photos/view-of-tesla-model-s-in-barcelona-spain-on-september-10-2018-picture-id1032050330?s=2048x2048",
+          },
+        ],
+      };
+      transporter.sendMail(message, function (err, info) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(info);
+          console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        }
+      });
+    });
   }
+
+  main().catch((error) => {
+    res.json({
+      error: error,
+    });
+  });
+
+  res.send("Email sent");
 });
+
+const PORT = 8080;
+
+app.listen(PORT, () => console.log(`Server connected on ${PORT}`));
